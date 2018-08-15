b122ed552dbd8cb0742066b13d91d50d9e2cb0e7
Pavel Zavoiskikh <hapass@ya.ru>
2018-08-15 23:32:21 +0300

    More test cases.

--- src/test/cpp/regex_lexer_test.hpp
+++ src/test/cpp/regex_lexer_test.hpp
@@ -7,10 +7,11 @@
 
 using namespace regex_parser;
 
-TEST(RegexLexer, lexer_reads_dot_from_expression) {
+TEST(RegexLexer, lexer_reads_symbols_from_expression) {
     RegexLexer lexer;
-    std::vector<Token> tokens = lexer.scan("a.b");
-    EXPECT_EQ(3, tokens.size());
+    std::vector<Token> tokens = lexer.scan("a.(x|y)?g+h*b");
+    
+    EXPECT_EQ(13, tokens.size());
 
     EXPECT_EQ(Letter, tokens[0].getType());
     EXPECT_EQ('a', tokens[0].getValue());
@@ -18,6 +19,36 @@ TEST(RegexLexer, lexer_reads_dot_from_expression) {
     EXPECT_EQ(Dot, tokens[1].getType());
     EXPECT_THROW(tokens[1].getValue(), IllegalStateException);
 
-    EXPECT_EQ(Letter, tokens[2].getType());
-    EXPECT_EQ('b', tokens[2].getValue());
+    EXPECT_EQ(OpeningBracket, tokens[2].getType());
+    EXPECT_THROW(tokens[2].getValue(), IllegalStateException);
+
+    EXPECT_EQ(Letter, tokens[3].getType());
+    EXPECT_EQ('x', tokens[3].getValue());
+
+    EXPECT_EQ(VerticalLine, tokens[4].getType());
+    EXPECT_THROW(tokens[4].getValue(), IllegalStateException);
+
+    EXPECT_EQ(Letter, tokens[5].getType());
+    EXPECT_EQ('y', tokens[5].getValue());
+
+    EXPECT_EQ(ClosingBracket, tokens[6].getType());
+    EXPECT_THROW(tokens[6].getValue(), IllegalStateException);
+
+    EXPECT_EQ(QuestionMark, tokens[7].getType());
+    EXPECT_THROW(tokens[7].getValue(), IllegalStateException);
+
+    EXPECT_EQ(Letter, tokens[8].getType());
+    EXPECT_EQ('g', tokens[8].getValue());
+
+    EXPECT_EQ(PlusSign, tokens[9].getType());
+    EXPECT_THROW(tokens[9].getValue(), IllegalStateException);
+
+    EXPECT_EQ(Letter, tokens[10].getType());
+    EXPECT_EQ('h', tokens[10].getValue());
+
+    EXPECT_EQ(Asterisk, tokens[11].getType());
+    EXPECT_THROW(tokens[11].getValue(), IllegalStateException);
+
+    EXPECT_EQ(Letter, tokens[12].getType());
+    EXPECT_EQ('b', tokens[12].getValue());
 }
